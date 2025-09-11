044a9f9e7005dbf301b478ec29fc7168fc2eb7c5
Brayden Corbridge <bbcorbridge@gmail.com>
2025-09-11 01:18:08 -0600

    end of module video lecture

--- modules/module6/assignment/app.js
+++ modules/module6/assignment/app.js
@@ -4,23 +4,23 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 // Modified from video due to handlebars being on newer version
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
 
 const app = express();
 
 // This is outdated in handlebars v6+
 // app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout'}));
 // The function below is what ChatGPT says is updated call
-app.engine(
-    "hbs",
-    engine({
-        extname: "hbs",
-        layoutsDir: "views/layouts",
-        defaultLayout: "main-layout",
-        extname: "hbs",
-    })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//     "hbs",
+//     engine({
+//         extname: "hbs",
+//         layoutsDir: "views/layouts",
+//         defaultLayout: "main-layout",
+//         extname: "hbs",
+//     })
+// );
+app.set('view engine', 'ejs');
 // This is default in express, set here for clarity
 app.set('views', 'views');
 

e9ec305c3f3f333318461c44b147524cc64df694
Brayden Corbridge <bbcorbridge@gmail.com>
2025-09-10 23:43:17 -0600

    video: Finishing the Pug Template

--- modules/module6/assignment/app.js
+++ modules/module6/assignment/app.js
@@ -6,6 +6,10 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+// This is default in express, set here for clarity
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -16,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);

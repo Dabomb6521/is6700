e77d189a13a6d8ff16655b25501c10a7f26386af
Brayden Corbridge <bbcorbridge@gmail.com>
2025-09-11 00:38:20 -0600

    video: Adding a Layout to Handlebars

--- modules/module6/assignment/app.js
+++ modules/module6/assignment/app.js
@@ -3,10 +3,24 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 const express = require('express');
+// Modified from video due to handlebars being on newer version
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// This is outdated in handlebars v6+
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout'}));
+// The function below is what ChatGPT says is updated call
+app.engine(
+    "hbs",
+    engine({
+        extname: "hbs",
+        layoutsDir: "views/layouts",
+        defaultLayout: "main-layout",
+        extname: "hbs",
+    })
+);
+app.set('view engine', 'hbs');
 // This is default in express, set here for clarity
 app.set('views', 'views');
 

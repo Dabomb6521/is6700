8ff5f647d9939f1258851808f3352d97c5decccd
Brayden Corbridge <bbcorbridge@gmail.com>
2025-10-02 02:49:21 -0600

    Finished module11 assignment2

--- modules/module11/assignment2/controllers/shop.js
+++ modules/module11/assignment2/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+
 const {Op} = require('sequelize');
 const bulkProducts = require('../data/bulk-add-products.json');
 
@@ -54,54 +54,110 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart
+    .getProducts()
+    .then(products => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: cartProducts
+        products: products
       });
-    });
-  });
+    })
+    .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err))
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts({where: {id: prodId}});
+  })
+  .then(products => {
+    let product;
+    if (products.length > 0) {
+      product = products[0];
+    }
+    if (product){
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity + 1;
+      return product
+    }
+    return Product.findByPk(prodId)
+  })
+  .then(product => {
+    return fetchedCart.addProduct(product, { through: { quantity: newQuantity }})
+  })
+  .then(() => {
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart.getProducts({where:{id: prodId}});
+  })
+  .then(products => {
+    const product = products[0];
+    return product.cartItem.destroy();
+  })
+  .then(result => {
     res.redirect('/cart');
-  });
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user
+    .createOrder()
+    .then(order => {
+      return order.addProducts(products.map(product => {
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      }));
+    })
+    .catch(err => console.log(err));
+  })
+  .then(result => {
+    fetchedCart.setProducts(null);
+  })
+  .then(result => {
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+  })
+  .catch(err => console.log(err1));
 };
 
 //Additional handler functions for last part of assignment
--- modules/module11/assignment2/util/database.js
+++ modules/module11/assignment2/util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 // *******  NOTE! ******** //
 // Replace database credentials/host below with your database credentials/host
 
-const sequelize = new Sequelize('YOUR_DB_NAME_HERE', 'YOUR_USER_NAME_HERE', 'YOUR_PASSWORD_HERE', {
+const sequelize = new Sequelize('5700braydencorbridge','5700braydencorbridge','A01885792', {
   dialect: 'mysql',
   host: 'hsb-web-dev.ckm1cfmd3i4j.us-west-2.rds.amazonaws.com'
 });

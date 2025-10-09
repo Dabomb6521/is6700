862053df9a6efc74610bfab36ff760884a1d835e
Brayden Corbridge <brayden.corbridge@sdl.usu.edu>
2025-10-09 09:49:55 -0600

    Midtearm: Add Friends

--- midterm/Q1/Q1.js
+++ midterm/Q1/Q1.js
@@ -1,1 +1,22 @@
 // Write the code for Question 1 here
+const myFriends = [
+  {
+    firstName: "Eric",
+    lastName: "Searl",
+    birthDate: new Date(1992, 1, 17),
+    favoriteFoods: ["pasta", "salad", "potatos"]
+  },
+  {
+    firstName: "Dallin",
+    lastName: "Southwick",
+    birthDate: 02-14-1994,
+    favoriteFoods: ["pasta", "salad", "potatos"]
+  },
+  {
+    firstName: "Stetzon",
+    lastName: "Blacker",
+    birthDate: 03/8/1993,
+    favoriteFoods: ["pasta", "salad", "potatos"]
+  }
+]
+

fe989ef394f873d1af2062a51474dd8b7e9b9474
Brayden Corbridge <bbcorbridge@gmail.com>
2025-10-11 17:37:15 -0600

    Finalize question 1

--- midterm/Q1/Q1.js
+++ midterm/Q1/Q1.js
@@ -3,20 +3,30 @@ const myFriends = [
   {
     firstName: "Eric",
     lastName: "Searl",
-    birthDate: new Date(1992, 1, 17),
+    birthDate: new Date(1992, 0, 17),
     favoriteFoods: ["pasta", "salad", "potatos"]
   },
   {
     firstName: "Dallin",
     lastName: "Southwick",
-    birthDate: new Date(1994, 2, 14),
+    birthDate: new Date(1994, 1, 14),
     favoriteFoods: ["pasta", "salad", "potatos"]
   },
   {
     firstName: "Stetzon",
     lastName: "Blacker",
-    birthDate: new Date(1993, 3, 8),
+    birthDate: new Date(1993, 2, 8),
     favoriteFoods: ["pasta", "salad", "potatos"]
   }
 ]
 
+function getFriendsInfo(friends) {
+  friends.forEach(friend => {
+    const age = new Date().getFullYear() - friend.birthDate.getFullYear();
+    const favFoodsJoined = friend.favoriteFoods.join(', ');
+
+    console.log(`${friend.firstName} ${friend.lastName} is ${age} years old and enjoys the following foods: ${favFoodsJoined}`);
+  });
+};
+
+getFriendsInfo(myFriends);

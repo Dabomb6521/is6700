aa43e5ba969312a8f83b702fc3c7bbaa06e9ca94
Brayden Corbridge <bbcorbridge@gmail.com>
2025-10-12 17:51:53 -0600

    Finalized question 2

--- midterm/Q2/Q2-callback.js
+++ midterm/Q2/Q2-callback.js
@@ -1,4 +1,39 @@
 // Write the code for Question 2a here
-import fs from 'fs';
+const { error } = require('console');
+const fs = require('fs');
 
-fs.writeFile()
+fs.writeFile('working.txt', 'Creating new file...\n', 'utf8', (err) => {
+    if (err) {
+        console.log("Error creating file: ", err);
+        return
+    }
+    console.log('File created!')
+
+    const extraText = "Some extra information that is super important " + "\n" + "This is multiple lines now so it must be extra special"
+
+    fs.appendFile('working.txt', extraText, (err) => {
+        if (err) {
+            console.log("Error creating file: ", err);
+            return
+        }
+        console.log("Appended lines to file!");
+
+        fs.readFile('working.txt','utf8', (err, data) => {
+            if (err) {
+                console.log("Error creating file: ", err);
+                return
+            }
+            console.log("File Content:\n", data);
+
+            fs.rename('working.txt', 'complete.txt', (err) => {
+                if (err) {
+                    console.log("Error creating file: ", err);
+                    return
+                }
+                console.log("Renamed file!");
+            });
+        });
+    });
+});
+
+
--- midterm/Q2/Q2-promise.js
+++ midterm/Q2/Q2-promise.js
@@ -1,1 +1,22 @@
 // Write the code for Question 2b here
+const fs = require('fs').promises
+
+const extraText = "Some extra information that is super important " + "\n" + "This is multiple lines now so it must be extra special"
+
+fs.writeFile('working.txt', 'Creating new file...\n', 'utf8')
+.then(() => {
+    console.log('File created!');
+    return fs.appendFile('working.txt', extraText);
+})
+.then(() => {
+    console.log("Appended lines to file!");
+    return fs.readFile('working.txt','utf8');
+})
+.then((data) => {
+    console.log("File Content:\n", data);
+    return fs.rename('working.txt', 'complete.txt');
+})
+.then(() => {
+    console.log("Renamed file!");
+})
+.catch(err => console.log("Error in file process: ", err));

e0719ee701a13bf136800800752f55e7f018ef79
Brayden Corbridge <brayden.corbridge@sdl.usu.edu>
2025-10-13 17:02:48 -0600

    finished Q3

--- midterm/mentor/mentor_node/app.js
+++ midterm/mentor/mentor_node/app.js
@@ -2,14 +2,25 @@ const express = require('express');
 const ejs = require('ejs');
 const path = requrie('path');
 const expressLayouts = require('express-ejs-layouts');
+const port = 3000;
+
+// Import Routes
+const homeRoutes = require('./controllers/home-routes');
 
 // Initialize the express app
 const app = express();
+
+// Initialize app settings
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // Mount Middleware
 
 // Static Resources
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
-console.log("Mentor App is running on port 3000");
+//  Register Routes
+app.get("/", homeRoutes);
+
+app.listen(port);
+console.log(`Mentor App is running on port ${port}`);
---
+++ midterm/mentor/mentor_node/controllers/home-controller.js
@@ -0,0 +1,8 @@
+
+exports.getHome = (req, res) => {
+    res.render('index', {title: "Home"});
+};
+
+exports.getAbout = (req, res) => {
+    res.render('about', {title: "About Us"})
+};

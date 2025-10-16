ee4729449c8d62bbe11ad2e1f6cd01f30f8d86af
Brayden Corbridge <brayden.corbridge@sdl.usu.edu>
2025-10-16 09:56:57 -0600

    Add code for index.ejs to pull from database

--- midterm/mentor/mentor_node/controllers/home-controller.js
+++ midterm/mentor/mentor_node/controllers/home-controller.js
@@ -1,6 +1,17 @@
+const Course = require('../models/course-model');
+const Event = require('../models/event-model');
+const Trainer = require('../models/trainer-model');
 
-exports.getHome = (req, res) => {
-    res.render('index', {title: "Home"});
+exports.getHome = async (req, res) => {
+    try {
+
+        res.render('index', {
+            title: 'Home',
+            courseCount: await Course.count(),
+            eventCount: await Event.count(),
+            trainerCount: await Trainer.count(),
+        });
+    } catch (err) {console.error(err);};
 };
 
 exports.getAbout = (req, res) => {
--- midterm/mentor/mentor_node/util/database.js
+++ midterm/mentor/mentor_node/util/database.js
@@ -1,13 +1,29 @@
 const Sequelize = require("sequelize");
 
+
+// Hosted AWS Instance
+// const sequelize = new Sequelize(
+//   "5700braydencorbridge_examproject",
+//   "5700braydencorbridge",
+//   "A01885792",
+//   {
+//     dialect: "mysql",
+//     host: "hsb-web-dev.ckm1cfmd3i4j.us-west-2.rds.amazonaws.com",
+//   }
+// );
+
+
+// Local Devcontainer
 const sequelize = new Sequelize(
   "5700braydencorbridge_examproject",
-  "5700braydencorbridge",
-  "A01885792",
+  "devuser",
+  "devpassword",
   {
     dialect: "mysql",
-    host: "hsb-web-dev.ckm1cfmd3i4j.us-west-2.rds.amazonaws.com",
+    host: "localhost",
+    port: "3306"
   }
 );
+
 
 module.exports = sequelize;

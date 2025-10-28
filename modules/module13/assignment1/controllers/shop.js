152a05939350e0fe3472ad59a862a7f53c31c5aa
Brayden Corbridge <bbcorbridge@gmail.com>
2025-10-28 09:51:13 -0600

    finish module 13, add extra code per assignment instructions

--- modules/module13/assignment1/controllers/shop.js
+++ modules/module13/assignment1/controllers/shop.js
@@ -1,16 +1,20 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
+const Order = require("../models/order");
+
+// Import data import file for Extra query practice
+const bulkProductsData = require('../data/bulk-add-products.json');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then(products => {
-      console.log(products)
-      res.render('shop/product-list', {
+    .then((products) => {
+      console.log(products);
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: "All Products",
+        path: "/products",
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
@@ -18,84 +22,185 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(product => {
-      res.render('shop/product-detail', {
+    .then((product) => {
+      res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: "/products",
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
   Product.find()
-    .then(products => {
-      res.render('shop/index', {
+    .then((products) => {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+        pageTitle: "Shop",
+        path: "/",
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: products
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
-    .then(product => {
+    .then((product) => {
       return req.user.addToCart(product);
     })
-    .then(result => {
+    .then((result) => {
       console.log(result);
-      res.redirect('/cart');
+      res.redirect("/cart");
     });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
-    .then(result => {
-      res.redirect('/cart');
+    .removeFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
-    .then(result => {
-      res.redirect('/orders');
+    .populate("cart.items.productId")
+    .then((user) => {
+      console.log(user.cart.items);
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+      order.save();
     })
-    .catch(err => console.log(err));
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
+  Order.find({ "user.userId": req.user._id })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
+};
+
+// Extra query practice requirements
+
+exports.queryPractice = (req, res, next) => {
+  res.render('shop/query-practice', {
+    path: '/query-practice',
+    pageTitle: 'Extra Practice'
+  });
+};
+
+exports.getBulkAdd = (req, res, next) => {
+  Product.create(bulkProductsData)
+  .then(result => {
+    console.log('Created Product');
+    res.redirect('/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
+exports.getStartWithC = (req, res, next) => {
+  Product.find({
+    title: {
+      $regex: /^C/, $options: 'i'
+    }
+  })
+  .then(products => {
+    res.render('shop/product-list', {
+      path: '/products',
+      pageTitle: 'Starts With C',
+      prods: products
+    })
+  })
+  .catch(err => console.log(err));
+};
+exports.getProductBetween = (req, res, next) => {
+  Product.find({
+    price: { $gte: 100, $lte: 200 }
+  })
+  .then(products => {
+    res.render('shop/product-list', {
+      path: '/products',
+      pageTitle: 'Price Between $100 and $200',
+      prods: products
+    })
+  })
+  .catch(err => console.log(err));
+};
+exports.getContainsYour = (req, res, next) => {
+  Product.find({
+    description: { $regex: /your/, $options: 'i' }
+  })
+  .then(products => {
+    res.render('shop/product-list', {
+      path: '/products',
+      pageTitle: 'Contains "your"',
+      prods: products
+    })
+  })
+  .catch(err => console.log(err));
+};
+exports.getBlenderPriceIncrease = (req, res, next) => {
+  Product.findOne({
+    title: { $regex: /blender/i }
+  })
+  .then(product => {
+    product.price = (product.price += 10).toFixed(2);
+    return product.save()
+  })
+  .then(result => {
+    console.log("Blender Price increased");
+    res.redirect('/products')
+  })
+  .catch(err => console.log(err));
+};
+exports.getDeleteAllProducts = (req, res, next) => {
+  Product.deleteMany({})
+  .then(result => {
+      console.log('Destroyed Product');
+      res.redirect('/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
--- modules/module13/assignment1/routes/shop.js
+++ modules/module13/assignment1/routes/shop.js
@@ -12,14 +12,24 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-// router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-// router.post('/create-order', shopController.postOrder);
+router.post('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
+
+router.get('/query-practice', shopController.queryPractice);
+
+// Extra Practice
+router.get('/query-practice/bulk-add', shopController.getBulkAdd)
+router.get('/query-practice/start-with-c', shopController.getStartWithC)
+router.get('/query-practice/product-between', shopController.getProductBetween)
+router.get('/query-practice/contains-your', shopController.getContainsYour)
+router.get('/query-practice/blender-price-increase', shopController.getBlenderPriceIncrease)
+router.get('/query-practice/delete-all-products', shopController.getDeleteAllProducts)
 
 module.exports = router;

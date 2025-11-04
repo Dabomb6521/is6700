66e7c50e20fdd48156e89c2ee9bdfdf0fb63de47
Brayden Corbridge <bbcorbridge@gmail.com>
2025-11-03 23:11:36 -0700

    mostly through module 14

---
+++ modules/module14/assignment1/controllers/auth.js
@@ -0,0 +1,27 @@
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+  console.log(req.session.isLoggedIn);
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("69097d1842a1100cd2d25498")
+      .then(user => {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        res.redirect("/");
+      })
+      .catch(err => console.log(err));
+  
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    req.redirect('/');
+  });
+};

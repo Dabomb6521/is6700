1a6770c7f25ad450522f104c67dc90df040678ef
Brayden Corbridge <bbcorbridge@gmail.com>
2025-11-11 10:27:01 -0700

    module 19 complete

--- modules/module19/assignment1/controllers/auth.js
+++ modules/module19/assignment1/controllers/auth.js
@@ -123,7 +123,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -168,8 +172,10 @@ exports.postSignup = (req, res, next) => {
       //   html: '<h1>You successfully signed up!</h1>'
       // });
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -225,9 +231,11 @@ exports.postReset = (req, res, next) => {
           `
         });
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
   });
 };
 
@@ -249,8 +257,10 @@ exports.getNewPassword = (req, res, next) => {
         passwordToken: token
       });
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -278,7 +288,9 @@ exports.postNewPassword = (req, res, next) => {
     .then(result => {
       res.redirect('/login');
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
